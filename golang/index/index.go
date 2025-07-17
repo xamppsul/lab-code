4c66c4016513c8d21e884a39586b8dfaf7e690b5
imlaura19 <lauraamalia2001@gmail.com>
2025-07-17 16:39:40 +0800

    perbaikan

---
+++ golang/index/index.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func introduction() {
+	fmt.Println("Hai i'm laura, software engineer golang")
+}
+func greating() {
+	fmt.Println("halo kak samsul")
+}
+func main() {
+	introduction()
+	greating()
+
+}
