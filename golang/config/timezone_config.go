c1bb28e67bad24f19aae268d1b6124b97c85dc69
xamppsul <samsul.marif100@gmail.com>
2025-07-29 21:08:23 +0800

    1. fix config in index.go 2. timezone config

--- golang/config/timezone_config.go
+++ golang/config/timezone_config.go
@@ -1,1 +1,18 @@
 package config
+
+import "time"
+
+var location *time.Location
+
+func SetTimezone(timezone string) error {
+	time_location, err := time.LoadLocation(timezone)
+	if err != nil {
+		panic(err.Error())
+	}
+	location = time_location
+	return nil
+}
+
+func GetTimezone(t time.Time) time.Time {
+	return t.In(location)
+}
--- golang/index.go
+++ golang/index.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"golang/config"
+	"log"
 	"os"
 	"runtime"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/qinains/fastergoding"
@@ -14,11 +16,18 @@ func main() {
 	config.EnvConfig("../golang/.env")
 	//hot reload
 	fastergoding.Run()
-
+	if err := config.SetTimezone(os.Getenv("GO_TIMEZONE")); err != nil {
+		log.Fatal(err)
+	}
+	//print timezone base location
+	time := config.GetTimezone(time.Now())
+	log.Print("Asia Jakarta :" + time.Format(os.Getenv("GO_TIMEZONE_FORMAT")))
+	//instance fiber load server
 	app := fiber.New()
 	app.Get("/", func(res *fiber.Ctx) error {
-		return res.SendString(runtime.Version() + " With fiber framework: " + os.Getenv("APP_DESCRIPTION"))
+		return res.SendString(runtime.Version() + " With fiber framework: " + os.Getenv("GO_DESCRIPTION"))
 	})
+	app.Listen(":" + os.Getenv("GO_PORT"))
+	//validate timezone location seting
 
-	app.Listen(":" + os.Getenv("APP_PORT"))
 }
